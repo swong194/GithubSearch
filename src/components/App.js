e24f8cb083d1f3293d3b8959fa065a8f3e30f4c4
Sunny Wong <swong194@gmail.com>
2018-03-31 10:21:14 -0400

    add search component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from "react";
+import { Provider } from "react-redux";
+import SearchContainer from "./search/SearchContainer";
 
-class App extends Component {
-  render() {
-    return <div>Hello World</div>;
-  }
-}
+const App = ({ store }) => {
+  return (
+    <Provider store={store}>
+      <SearchContainer />
+    </Provider>
+  );
+};
 
 export default App;
