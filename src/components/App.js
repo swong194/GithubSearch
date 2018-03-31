fbb902882bc05e978ab2374bd63385da6ef787f9
Sunny Wong <swong194@gmail.com>
2018-03-31 10:43:14 -0400

    add react-router-dom

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
-import React, { Component } from "react";
+import React from "react";
 import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 import SearchContainer from "./search/SearchContainer";
 
 const App = ({ store }) => {
   return (
     <Provider store={store}>
-      <SearchContainer />
+      <BrowserRouter>
+        <SearchContainer />
+      </BrowserRouter>
     </Provider>
   );
 };
