51a435d8e2b134ad06b09c39562508dc2169ae34
Sunny Wong <swong194@gmail.com>
2018-03-31 09:23:07 -0400

    setup store

---
+++ src/store/configureStore.js
@@ -0,0 +1,15 @@
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+
+import rootReducer from "../reducers/rootReducer";
+
+const configureStore = ({ preLoadedState }) => {
+  return createStore(
+    rootReducer,
+    preLoadedState,
+    applyMiddleware(thunk, logger)
+  );
+};
+
+export default configureStore;
