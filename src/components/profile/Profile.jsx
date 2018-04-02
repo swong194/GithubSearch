b566e6c7a08b05c322ea75375379f7676a1a1b75
Sunny Wong <swong194@gmail.com>
2018-04-02 11:30:39 -0400

    add style to profile component

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,14 +1,26 @@
 import React, { Component } from "react";
+import { Redirect } from "react-router";
+import "./Profile.css";
 
 export default class Profile extends Component {
   render() {
     const { login, avatar_url, followers } = this.props.profile;
+    if (!login) {
+      return <Redirect to="/" />;
+    }
     return (
-      <section>
-        <img src={avatar_url} alt="profile picture" />
-        <p>
-          {login} is followed by {followers} users
-        </p>
+      <section className="profile">
+        <div>
+          <img src={avatar_url} alt="profile picture" />
+        </div>
+        <div>
+          <p>
+            {login} is followed by {followers} users
+          </p>
+          <a target="_blank" href="https://github.com/swong194">
+            {login}'s github
+          </a>
+        </div>
       </section>
     );
   }
